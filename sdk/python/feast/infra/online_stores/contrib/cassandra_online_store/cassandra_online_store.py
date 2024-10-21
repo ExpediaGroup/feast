238b38913edaa6fcbf25a4c8f63605b73cb42009
Zach Barnett <zbarnett99@gmail.com>
2024-10-21 10:08:45 -0500

    fix: address Bhargav's comments

--- sdk/python/feast/infra/online_stores/contrib/cassandra_online_store/cassandra_online_store.py
+++ sdk/python/feast/infra/online_stores/contrib/cassandra_online_store/cassandra_online_store.py
@@ -88,7 +88,7 @@ CREATE_TABLE_CQL_TEMPLATE = """
         event_ts        TIMESTAMP,
         created_ts      TIMESTAMP,
         PRIMARY KEY ((entity_key), feature_name)
-    ) WITH CLUSTERING ORDER BY (feature_name ASC) {optional_ttl_clause};
+    ) WITH CLUSTERING ORDER BY (feature_name ASC);
 """
 
 DROP_TABLE_CQL_TEMPLATE = "DROP TABLE IF EXISTS {fqtable};"
@@ -231,25 +231,14 @@ class CassandraOnlineStore(OnlineStore):
             return self._session
         if not self._session:
             # configuration consistency checks
+            hosts = online_store_config.hosts
             secure_bundle_path = online_store_config.secure_bundle_path
-            port = (
-                19042
-                if online_store_config.type == "scylladb"
-                else (online_store_config.port or 9042)
-            )
+            port = online_store_config.port or 9042
             keyspace = online_store_config.keyspace
             username = online_store_config.username
             password = online_store_config.password
             protocol_version = online_store_config.protocol_version
-            if online_store_config.type == "scylladb":
-                if online_store_config.load_balancing is None:
-                    online_store_config.load_balancing = CassandraOnlineStoreConfig.CassandraLoadBalancingPolicy(
-                        load_balancing_policy="TokenAwarePolicy(DCAwareRoundRobinPolicy)"
-                    )
-                if online_store_config.protocol_version is None:
-                    protocol_version = 4
 
-            hosts = online_store_config.hosts
             db_directions = hosts or secure_bundle_path
             if not db_directions or not keyspace:
                 raise CassandraInvalidConfig(E_CASSANDRA_NOT_CONFIGURED)
@@ -579,10 +568,7 @@ class CassandraOnlineStore(OnlineStore):
         fqtable = CassandraOnlineStore._fq_table_name(keyspace, project, table)
         create_cql = self._get_cql_statement(config, "create", fqtable)
         logger.info(f"Creating table {fqtable}.")
-        if config.online_config.ttl:
-            session.execute(create_cql, parameters=config.online_config.ttl)
-        else:
-            session.execute(create_cql)
+        session.execute(create_cql)
 
     def _get_cql_statement(
         self, config: RepoConfig, op_name: str, fqtable: str, **kwargs
@@ -599,13 +585,9 @@ class CassandraOnlineStore(OnlineStore):
         """
         session: Session = self._get_session(config)
         template, prepare = CQL_TEMPLATE_MAP[op_name]
-        if op_name == "create" and config.online_config.ttl:
-            ttl_clause = " WITH default_time_to_live = ?"
-        else:
-            ttl_clause = None
-
         statement = template.format(
-            fqtable=fqtable, optional_ttl_clause=ttl_clause, **kwargs
+            fqtable=fqtable,
+            **kwargs,
         )
         if prepare:
             # using the statement itself as key (no problem with that)
