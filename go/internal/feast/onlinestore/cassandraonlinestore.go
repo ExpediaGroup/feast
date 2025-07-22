c69228136e18491d28f3d2fb8224d4573bf2e4e3
omiranda <omiranda@expediagroup.com>
2025-07-21 20:47:35 -0500

    fix: Handle features returned as string in cassandra store

--- go/internal/feast/onlinestore/cassandraonlinestore.go
+++ go/internal/feast/onlinestore/cassandraonlinestore.go
@@ -771,7 +771,18 @@ func (c *CassandraOnlineStore) OnlineReadRange(ctx context.Context, groupedRefs 
 						}
 					} else {
 						if valueStr, ok := readValues[featName]; ok {
-							val, status, err = UnmarshalStoredProto(valueStr.([]byte))
+							switch v := valueStr.(type) {
+							case []byte:
+								val, status, err = UnmarshalStoredProto(v)
+							case string:
+								log.Warn().
+									Str("feature", featName).
+									Msg("Converting string to []byte for Cassandra value")
+								val, status, err = UnmarshalStoredProto([]byte(v))
+							default:
+								errorsChannel <- fmt.Errorf("unexpected type for feature value: %T", valueStr)
+								return
+							}
 							if err != nil {
 								errorsChannel <- err
 								return
