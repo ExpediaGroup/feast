3e30f9ec32593bd9181e9d3ea4fc7acf9079eb19
omiranda <omiranda@expediagroup.com>
2025-07-28 01:10:38 -0500

    try to unmarshall as string

--- go/internal/feast/onlinestore/unmarshal_utils.go
+++ go/internal/feast/onlinestore/unmarshal_utils.go
@@ -1,7 +1,6 @@
 package onlinestore
 
 import (
-	"fmt"
 	"github.com/feast-dev/feast/go/protos/feast/serving"
 	"github.com/feast-dev/feast/go/protos/feast/types"
 	"google.golang.org/protobuf/proto"
@@ -9,24 +8,31 @@ import (
 )
 
 func UnmarshalStoredProto(valueStr []byte) (*types.Value, serving.FieldStatus, error) {
+	if len(valueStr) == 0 {
+		return &types.Value{Val: &types.Value_NullVal{NullVal: types.Null_NULL}},
+			serving.FieldStatus_NULL_VALUE,
+			nil
+	}
+
 	var message types.Value
 	null := &types.Value{Val: &types.Value_NullVal{NullVal: types.Null_NULL}}
 	if err := proto.Unmarshal(valueStr, &message); err != nil {
-		return nil, serving.FieldStatus_INVALID, fmt.Errorf("error converting parsed online store Value to types.Value: %w", err)
+		return &types.Value{Val: &types.Value_StringVal{StringVal: string(valueStr)}},
+			serving.FieldStatus_PRESENT,
+			nil
 	}
 	if message.Val == nil {
 		return null, serving.FieldStatus_NULL_VALUE, nil
-	} else {
-		switch message.Val.(type) {
-		case *types.Value_UnixTimestampVal:
-			// null timestamps are read as min int64, so we convert them to nil
-			if message.Val.(*types.Value_UnixTimestampVal).UnixTimestampVal == math.MinInt64 {
-				return null, serving.FieldStatus_NULL_VALUE, nil
-			} else {
-				return &types.Value{Val: message.Val}, serving.FieldStatus_PRESENT, nil
-			}
-		default:
-			return &types.Value{Val: message.Val}, serving.FieldStatus_PRESENT, nil
+	}
+
+	switch message.Val.(type) {
+	case *types.Value_UnixTimestampVal:
+		// null timestamps are read as min int64, so we convert them to nil
+		if message.Val.(*types.Value_UnixTimestampVal).UnixTimestampVal == math.MinInt64 {
+			return null, serving.FieldStatus_NULL_VALUE, nil
 		}
+		return &types.Value{Val: message.Val}, serving.FieldStatus_PRESENT, nil
+	default:
+		return &types.Value{Val: message.Val}, serving.FieldStatus_PRESENT, nil
 	}
 }
